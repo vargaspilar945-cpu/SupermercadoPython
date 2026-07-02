5a9c1fe4cd00835147f26157ab36ac4c1866dea5
rodriguezbrisa <rodriguezbrisa0503@gmail.com>
2026-07-02 18:46:29 -0300

    Create estadisticas.py with order counting function
    agrego función cantidad_pedidos

---
+++ estadisticas.py
@@ -0,0 +1,10 @@
+Módulo de ESTADÍSTICAS - Sistema de Pedidos de Comida
+--------------------------------------------------------
+Responsable: Brisa Alanis Rodriguez
+
+# ---------- FUNCIONES DE CÁLCULO ----------
+ 
+def cantidad_pedidos(pedidos):
+    """Devuelve cuántos pedidos se registraron."""
+    return len(pedidos)
+
